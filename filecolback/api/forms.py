931122c29e017d7ad12c3c965a19360c568e67f9
hoijoii <oodh3328@gmail.com>
2022-06-02 23:12:31 +0900

    Update api/forms.py : form 파일 주석 삭제

--- filecolback/api/forms.py
+++ filecolback/api/forms.py
@@ -1,10 +1,5 @@
 from django import forms
 from .models import File
-'''
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, Row, Column, Field
-'''
-
 
 class DocumentForm(forms.ModelForm):
     class Meta:
