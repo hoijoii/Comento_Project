4be720e51e62d5031527464404ae3e7043400cd7
hoijoii <oodh3328@gmail.com>
2022-06-03 13:36:31 +0900

    Update account/views.py : 안 쓰는 코드  주석처리

--- filecolback/account/views.py
+++ filecolback/account/views.py
@@ -1,11 +1,13 @@
 from .models import User
 from .serializers import UserSerializer
 from rest_framework import viewsets
+'''
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+'''
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
