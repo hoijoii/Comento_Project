4213e0807b78c33baab7d44d404e8cf4a377b9d8
hoijoii <oodh3328@gmail.com>
2022-06-06 00:38:50 +0900

    [Feat] : detail 페이지 filelist 업로드 날짜 표기 변경
    날짜,시간 표기 -> 날짜만 표기(file.upload_at.substring(10, 0))

--- file-col/src/components/FileList.js
+++ file-col/src/components/FileList.js
@@ -6,7 +6,6 @@ import "./filelist.css";
 
 const FileList = ({ postId }) => {
   const [files, setFiles] = useState([]);
-  const [isFileList, setFileList] = useState(false);
   const [users, setUsers] = useState([]);
   const [token] = useCookies("mytoken");
 
@@ -27,54 +26,41 @@ const FileList = ({ postId }) => {
         },
       })
       .then((resp) => setFiles(resp.data))
-      .then(handleFileList)
       .catch((Error) => {
         console.log(Error);
       });
   }, []);
 
-  const handleFileList = () => {
-    console.log(files.length);
-    if (files.length !== 0) {
-      console.log(isFileList);
-      setFileList(true);
-    }
-  };
-
   return (
     <div className="fileTable">
-      {isFileList ? (
-        <Table responsive>
-          <thead className="head">
-            <tr>
-              <th>제목</th>
-              <th>업로드 날짜</th>
-              <th>작성자</th>
-            </tr>
-          </thead>
-          <tbody>
-            {files.map((file) => {
-              if (file.post === JSON.parse(postId)) {
-                return (
-                  <tr key={file.id}>
-                    <td>{file.title}</td>
-                    <td>{file.upload_at}</td>
-                    <td className="bodyUser">
-                      {users.map((user) => {
-                        if (user.id === file.uploader) {
-                          return user.realname;
-                        }
-                      })}
-                    </td>
-                  </tr>
-                );
-              }
-            })}
-          </tbody>
-        </Table>
-      ) : (
-        <div></div>
-      )}
+      <Table responsive>
+        <thead className="head">
+          <tr>
+            <th>제목</th>
+            <th>업로드 날짜</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map((file) => {
+            if (file.post === JSON.parse(postId)) {
+              return (
+                <tr key={file.id}>
+                  <td>{file.title}</td>
+                  <td className="body">{file.upload_at.substring(10, 0)}</td>
+                  <td className="body">
+                    {users.map((user) => {
+                      if (user.id === file.uploader) {
+                        return user.realname;
+                      }
+                    })}
+                  </td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 };
